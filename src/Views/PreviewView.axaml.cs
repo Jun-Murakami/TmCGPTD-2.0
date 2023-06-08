116ccaa8a4e73523e2e0c19f6c2fcd02639866c7
Jun Murakami <contact@bucketrelay.com>
2023-06-09 00:05:09 +0900

    forV2.4.2

--- src/Views/PreviewView.axaml.cs
+++ src/Views/PreviewView.axaml.cs
@@ -14,8 +14,17 @@ namespace TmCGPTD.Views
             PreviewViewModel.EditorViewModel = editorViewModel;
             DataContext = PreviewViewModel;
             VMLocator.PreviewViewModel = PreviewViewModel;
+
+            var previewTextBox = this.FindControl<TextBox>("PreviewTextBox");
+
+            this.AttachedToVisualTree += (sender, e) =>
+            {
+                if (previewTextBox == null || VMLocator.EditorViewModel == null|| VMLocator.EditorViewModel.GetRecentText() == null) return;
+                previewTextBox.Text = VMLocator.EditorViewModel.GetRecentText();
+            };
         }
-            private void InitializeComponent()
+
+        private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
         }
