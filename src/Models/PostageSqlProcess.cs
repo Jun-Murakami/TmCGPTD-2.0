be52ee37831a59d1da2478846365e7539ebc4943
Jun Murakami <contact@bucketrelay.com>
2023-06-26 11:37:52 +0900

    Test

---
+++ src/Models/PostageSqlProcess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TmCGPTD.Models
+{
+    public class PostageSqlProcess
+    {
+        public class ChatRoom
+        {
+
+        }
+    }
+}
