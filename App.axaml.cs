54f1614305aae5fcb522d8d4e291443befbde1de
Jun Murakami <contact@bucketrelay.com>
2023-07-17 21:11:48 +0900

    forV2.5

--- App.axaml.cs
+++ App.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Markup.Xaml.Styling;
+using System;
 using TmCGPTD.Views;
 
 namespace TmCGPTD
@@ -10,6 +12,15 @@ namespace TmCGPTD
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
+            if (OperatingSystem.IsMacOS())
+            {
+                this.Styles.Add(new StyleInclude(new Uri("avares://TmCGPTD/Assets/StylesMac.axaml")));
+            }
+            else
+            {
+                this.Styles.Add(new StyleInclude(new Uri("avares://AvaloniaEdit/Themes/Fluent/AvaloniaEdit.xaml")));
+                this.Styles.Add(new StyleInclude(new Uri("avares://TmCGPTD/Assets/Styles.axaml")));
+            }
         }
 
         public override void OnFrameworkInitializationCompleted()
